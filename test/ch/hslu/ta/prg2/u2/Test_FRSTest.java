e089f5a6ccd555fcb6ef821a454d73ec1ad47523
Stefan Friedli <stefan.friedli93@gmail.com>
2015-10-01 11:26:16 +0200

    test Junit

---
+++ test/ch/hslu/ta/prg2/u2/Test_FRSTest.java
@@ -0,0 +1,12 @@
+package ch.hslu.ta.prg2.u2;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Stefan on 01.10.2015.
+ *
+ * @author Stefan Friedil(stefan.friedli@stud.hslu.ch)
+ */
+public class Test_FRSTest {
+
+}
